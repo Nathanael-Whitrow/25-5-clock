2d20a92b4f8731364637ab271f3763cb91e3f971
Nathanael Whitrow <nathanael.whitrow@gmail.com>
2022-11-18 14:52:24 +1030

    Remove defaults and starting classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Title() {
+  return (
+    <div>
+      <h1>25 + 5 Clock</h1>
+    </div>
+  )
+}
+
+function TimeAdjustor() {
+  return (
+    <div>
+      <div>
+        <span>Break length</span>
+      </div>
+      <div>
+        <button></button>
+        Some text
+        <button></button>
+      </div>
+    </div>
+  )
+}
+
+function Timer() {
+  return (
+    <div>
+      <div>
+        <h2>mm:ss</h2>
+      </div>
+    </div>
+  )
+}
+
+function Controls() {
+  const text = "> || <>"
+  return (
+    <div>
+      <h2>{text}</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <span>
+        <TimeAdjustor />
+        <TimeAdjustor />
+      </span>
+      <Timer />
+      <Controls />
     </div>
   );
 }
