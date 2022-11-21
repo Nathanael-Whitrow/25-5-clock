0f9829bb9797abbcef3f7d0654af873c48a43476
Nathanael Whitrow <nathanael.whitrow@gmail.com>
2022-11-21 13:10:41 +1030

    Finish layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause, faArrowRotateLeft } from '@fortawesome/free-solid-svg-icons';
 
 function Title() {
   return (
@@ -8,16 +10,14 @@ function Title() {
   )
 }
 
-function TimeAdjustor() {
+function TimeAdjustor(props) {
   return (
-    <div>
-      <div>
-        <span>Break length</span>
-      </div>
-      <div>
-        <button></button>
-        Some text
-        <button></button>
+    <div id="time-adjustor">
+      <p className="title-2">{props.text}</p>
+      <div className='count-controls'>
+        <button className='button button-up'><i className='fa-solid fa-arrow-up'></i></button>
+        <span className='counter'>Count</span>
+        <button className='button button-down'><i className='fa-solid fa-arrow-down'></i></button>
       </div>
     </div>
   )
@@ -27,17 +27,18 @@ function Timer() {
   return (
     <div>
       <div>
-        <h2>mm:ss</h2>
+        <p className='timer-text'>mm:ss</p>
       </div>
     </div>
   )
 }
 
 function Controls() {
-  const text = "> || <>"
   return (
-    <div>
-      <h2>{text}</h2>
+    <div className='controls'>
+      <button className='button controls-play'><FontAwesomeIcon icon={faPlay} /></button>
+      <button className='button controls-pause'><FontAwesomeIcon icon={faPause} /></button>
+      <button className='button controls-restart'><FontAwesomeIcon icon={faArrowRotateLeft} /></button>
     </div>
   )
 }
@@ -46,9 +47,9 @@ function App() {
   return (
     <div className="App">
       <Title />
-      <span>
-        <TimeAdjustor />
-        <TimeAdjustor />
+      <span id="timers">
+        <TimeAdjustor text='Break Time' />
+        <TimeAdjustor text='Session Time' />
       </span>
       <Timer />
       <Controls />
