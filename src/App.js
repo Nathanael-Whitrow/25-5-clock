17824b00646d792131d738a9fcc9fa0d80d71e08
Nathanael Whitrow <nathanael.whitrow@gmail.com>
2022-11-22 14:04:46 +1030

    Add functionality for break and session time

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause, faArrowRotateLeft } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,13 +12,14 @@ function Title() {
 }
 
 function TimeAdjustor(props) {
+
   return (
     <div id="time-adjustor">
       <p className="title-2">{props.text}</p>
       <div className='count-controls'>
-        <button className='button button-up'><i className='fa-solid fa-arrow-up'></i></button>
-        <span className='counter'>Count</span>
-        <button className='button button-down'><i className='fa-solid fa-arrow-down'></i></button>
+        <button className='button button-up' onClick={() => props.setTime(props.time + 1)}><i className='fa-solid fa-arrow-up'></i></button>
+        <span className='counter'>{props.time}</span>
+        <button className='button button-down' onClick={() => props.setTime(props.time - 1)}><i className='fa-solid fa-arrow-down'></i></button>
       </div>
     </div>
   )
@@ -31,7 +33,7 @@ function Timer() {
   )
 }
 
-function Controls() {
+function PlayControls() {
   return (
     <div className='controls'>
       <button className='button controls-play'><FontAwesomeIcon icon={faPlay} /></button>
@@ -42,16 +44,19 @@ function Controls() {
 }
 
 function App() {
+  const [breakTime, setBreakTime] = useState(5);
+  const [sessionTime, setSessionTime] = useState(25);
+
   return (
     <div className="App">
       <Title />
       <span id="timers">
-        <TimeAdjustor text='Break Time' />
-        <TimeAdjustor text='Session Time' />
+        <TimeAdjustor text='Break Time' time={breakTime} setTime={setBreakTime} />
+        <TimeAdjustor text='Session Time' time={sessionTime} setTime={setSessionTime} />
       </span>
       <div className='timer-box'>
         <Timer />
-        <Controls />
+        <PlayControls />
       </div>
     </div>
   );
